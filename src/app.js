14bfed7f60dbbe4552d7b849c046d23efb60b771
illia lukianov <lykianovib@gmail.com>
2016-12-06 19:57:04 +0200

    Webpack setup

---
+++ src/app.js
@@ -0,0 +1,2 @@
+require('mainStyles');
+require('stack');
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  context: __dirname + '/src', // path prepend to entry option
+  entry: {
+    bundle: './app.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js'
+  },
+  module: {
+    preLoaders: [],
+    postLoaders: [],
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style','css']
+      },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    ]
+  },
+  resolve:{
+    alias: {
+      stack: '../src/js/stack',
+      mainStyles: '../src/css/main.css'
+    }
+  }
+};
